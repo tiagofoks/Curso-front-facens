4b02fc0d401e062dd80898c6d1451197a6e4ac1a
TIAGO FOKS <TIAGO FOKS>
2016-07-29 20:30:13 -0300

    adiciona js para scroll da barra

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	var nav = document.querySelector('nav');
+	var links = document.querySelectorAll('nav a');
+
+	console.log('e aí cara', nav, links);
+
+	links.click(function(){
+		var target = $(this.hash);
+
+		$('html, body').animate({
+			scrollTop: target.offset().top
+		}, 500);
+
+
+		return false;
+
+
+
+	})
+})
+
